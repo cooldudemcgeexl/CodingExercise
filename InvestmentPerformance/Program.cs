b323508f36a92769a81de32d834d299785d86d8b
Nick Lay <layns@mail.uc.edu>
2025-01-06 18:44:37 -0500

    add seed data and seeding logic

--- InvestmentPerformance/Program.cs
+++ InvestmentPerformance/Program.cs
@@ -1,18 +1,45 @@
 using Microsoft.EntityFrameworkCore;
 using InvestmentPerformance.Models;
+using InvestmentPerformance.Utils;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<IPContext>(opt => opt.UseInMemoryDatabase("InvestmentPerformance"));
+builder.Services.AddDbContext<IPContext>(opt => opt.UseInMemoryDatabase("InvestmentPerformance").UseSeeding((context, _) =>
+{
+
+    context.Set<User>().AddRange(SeedData.Users);
+    context.SaveChanges();
+
+    context.Set<Investment>().AddRange(SeedData.Investments);
+    context.SaveChanges();
+
+}).UseAsyncSeeding(async (context, _, CancellationToken) =>
+{
+
+    context.Set<User>().AddRange(SeedData.Users);
+    await context.SaveChangesAsync(CancellationToken);
+
+
+    context.Set<Investment>().AddRange(SeedData.Investments);
+    await context.SaveChangesAsync(CancellationToken);
+}));
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+
+// Ensure the database schema exists. This will cause the seeding logic to be called.
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<IPContext>();
+    context.Database.EnsureCreated();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
---
+++ InvestmentPerformance/Utils/SeedData.cs
@@ -0,0 +1,15 @@
+
+using InvestmentPerformance.Models;
+
+namespace InvestmentPerformance.Utils;
+public static class SeedData
+{
+    public static List<User> Users = [
+        new User {Id = 1, FirstName = "Nick", LastName = "Lay"}
+    ];
+
+
+    public static List<Investment> Investments = [
+        new Investment {Id = 1, UserId=1, Name = "Alan Smithee Productions", NumShares = 45, CostBasisPerShare = 102.57M, CurrentPrice= 100.00M}
+    ];
+};
