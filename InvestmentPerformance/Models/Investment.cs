11e74f6e1c7ec023367933a8b71181cf8762e2d2
Nick Lay <layns@mail.uc.edu>
2025-01-07 16:26:11 -0500

    simplify ivestment models

--- InvestmentPerformance/Models/Investment.cs
+++ InvestmentPerformance/Models/Investment.cs
@@ -8,12 +8,9 @@ public class Investment
     public int NumShares { get; set; }
     public decimal CostBasisPerShare { get; set; }
     public decimal CurrentPrice { get; set; }
-    public decimal CurrentValue => NumShares * CurrentPrice;
 
-    public decimal TotalGainLoss => CurrentValue - (NumShares * CostBasisPerShare);
 
     public DateTime PurchaseDate { get; set; }
-    public string InvestmentTerm => DateTime.Now > PurchaseDate.AddYears(1) ? "Long" : "Short";
 
     public int UserId { get; set; }
     public User User { get; set; } = null!;
@@ -27,20 +24,18 @@ public class InvestmentDetails
     public string? Name { get; set; }
     public int NumShares { get; set; }
     public decimal CostBasisPerShare { get; set; }
-    public decimal CurrentValue { get; set; }
+    public decimal CurrentValue => NumShares * CurrentPrice;
     public decimal CurrentPrice { get; set; }
     public string InvestmentTerm { get; set; }
-    public decimal TotalGainLoss { get; set; }
+    public decimal TotalGainLoss => CurrentValue - (NumShares * CostBasisPerShare);
 
     public InvestmentDetails(Investment investment)
     {
         Name = investment.Name;
         NumShares = investment.NumShares;
         CostBasisPerShare = investment.CostBasisPerShare;
-        CurrentValue = investment.CurrentValue;
         CurrentPrice = investment.CurrentPrice;
-        InvestmentTerm = investment.InvestmentTerm;
-        TotalGainLoss = investment.TotalGainLoss;
+        InvestmentTerm = DateTime.Now > investment.PurchaseDate.AddYears(1) ? "Long" : "Short";
     }
 }
 
